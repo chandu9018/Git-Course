098d294cfe75bbfa01a8d5e9ab99ba598fc8ddf0
chandu <pradeepsatya222@gmail.com>
2025-06-24 15:55:53 +0530

    Version 1.1

--- ServerConnect.py
+++ ServerConnect.py
@@ -3,3 +3,6 @@ api_url = "https://jsonplaceholder.typicode.com/todos/1"
 response = requests.get(api_url)
 print(response.status_code)
 print(response.json())
+print(response.json()['userId'])
+print(response.json()['title'])
+print(response.json()['completed'])
